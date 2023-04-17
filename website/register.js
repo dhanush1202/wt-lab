e53092d934200bd2bf153d342644b838b3e0505d
dhanush <dhanushg25@gmail.com>
2023-04-18 01:50:01 +0530

    form-updated

---
+++ website/register.js
@@ -0,0 +1,14 @@
+function validate(){
+    let name = document.registerform.name.value;
+    let password = document.registerform.password.value;
+    let email = document.registerform.email.value;
+    let phone = document.registerform.phone.value;
+    let sex = document.registerform.sex.value;
+    let date = document.registerform.date.value;
+    let language = document.registerform.language.value;
+    let address = document.registerform.address.value;
+    var x;
+    console.log( language);
+
+    
+}
